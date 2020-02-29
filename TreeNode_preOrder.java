6b37512ff71df4fb1ace1c2ff5e4a3901bb33f67
wannawannawanna <38182423+wannawannawanna@users.noreply.github.com>
2020-02-29 19:53:48 +0800

    Update TreeNode_preOrder.java

--- TreeNode_preOrder.java
+++ TreeNode_preOrder.java
@@ -29,7 +29,7 @@ public class PreorderReverse {
 //		return res;
 //	}
 	
-//	非递归法
+//	非递归法1
 	public ArrayList<Integer> preOrderReverse(TreeNode root){
 		ArrayList<Integer> res = new ArrayList<Integer>();
 		if(root == null) {
@@ -45,6 +45,29 @@ public class PreorderReverse {
 			}
 			if(node.left != null) {
 				stack.push(node.left);
+			}
+			
+		}
+		return res;
+	}
+//非递归法2
+	public ArrayList<Integer> preOrderReverse(TreeNode root){
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		if(root == null) {
+			return res;
+		}
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(root);
+		TreeNode cur = root;
+		while(!stack.isEmpty() || cur != null) {
+			if(cur != null) {
+				res.add(cur.val);
+				stack.push(cur);
+				cur = cur.left;
+			}
+			else {
+				cur = stack.pop();
+				cur = cur.right;
 			}
 			
 		}
