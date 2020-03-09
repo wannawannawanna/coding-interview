d71238c0fc8d87afbeede4bf02bd6b695d25a855
wannawannawanna <38182423+wannawannawanna@users.noreply.github.com>
2020-03-09 20:27:51 +0800

    Create 55_00_00_TreeDepth.java

---
+++ 55_00_00_TreeDepth.java
@@ -0,0 +1,51 @@
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+import java.util.Queue;
+import java.util.LinkedList;
+public class Solution {
+     //递归
+    public int TreeDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int nLeft = TreeDepth(root.left);
+        int nRight = TreeDepth(root.right);
+        return (nLeft > nRight)?(nLeft + 1):(nRight + 1);
+    }
+    //非递归,还不太懂
+    public int TreeDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int depth = 0, count = 0, nextCount = 1;
+        while(queue.size()!=0){
+            TreeNode top = queue.poll();
+            count++;
+            if(top.left != null){
+                queue.add(top.left);
+            }
+            if(top.right != null){
+                queue.add(top.right);
+            }
+            if(count == nextCount){
+                nextCount = queue.size();
+                count = 0;
+                depth++;
+            }
+        }
+        return depth;
+    }
+}
