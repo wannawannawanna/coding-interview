c0f403624514d143062b8a83ce30fff9fd28b3c1
wannawannawanna <38182423+wannawannawanna@users.noreply.github.com>
2020-03-09 22:26:56 +0800

    Create 32_00_00_PrintFromTopToBottom.java

---
+++ 32_00_00_PrintFromTopToBottom.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Deque;
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        Deque<TreeNode> queue = new LinkedList<TreeNode>();
+        if(root == null){
+            return array;
+        }
+        queue.add(root);
+        while(queue.size() != 0){
+            TreeNode pCurrent = queue.poll();
+            array.add(pCurrent.val);
+            if(pCurrent.left != null){
+                queue.add(pCurrent.left);
+                
+            }
+            if(pCurrent.right != null){
+                queue.add(pCurrent.right);
+                
+            }
+        }
+        return array;
+    }
+}
