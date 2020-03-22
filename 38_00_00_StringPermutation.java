aa9811257360cbb7c1468efadea96f9e2edafec4
wannawannawanna <38182423+wannawannawanna@users.noreply.github.com>
2020-03-22 12:06:41 +0800

    Update 38_00_00_StringPermutation.java

--- 38_00_00_StringPermutation.java
+++ 38_00_00_StringPermutation.java
@@ -38,3 +38,32 @@ public class Solution {
         cs[j] = temp;
     }
 }
+
+
+
+// 字符串的组合
+
+// 解法一
+// 位计算法
+// 若字符串长度为n, 则所有组合之和为C(1,n)+C(2,n)+...+C(n,n)=2^(n)-1;
+public ArrayList<String> Combination(String str){
+    ArrayList<String> result = new ArrayList<String>();
+    if(str == null || str.length() == 0){
+        return result;
+    }
+    int length = str.length();
+    int n = 1 << length;   // 2^n
+    for(int i = 1; i < n; i++){  //2^0不用算,所有状态加起来是2^n-1,所以1到2^n
+        String s;
+        for(int j = 0; j < length; j++){  //将1到2^n-1用二进制表示，将所有位上的1都找出来
+            int temp = i;
+            if(i & (1 << j)){ //如果j位是1，就会被找出来
+                s += str.charAt(j);
+            }
+        }
+        result.add(s);
+    }
+    Collections.sort(result); //保证输出是有序的
+    return result;
+    
+}
