93466a5f1c41e466df3f01a75ad5922176b6aa41
wannawannawanna <38182423+wannawannawanna@users.noreply.github.com>
2020-04-01 22:08:13 +0800

    Create 26_00_00_SubTree.java

---
+++ 26_00_00_SubTree.java
@@ -0,0 +1,48 @@
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+       
+        boolean result = false;
+        //当Tree1和Tree2都不为零的时候，才进行比较。否则直接返回false
+        if(root1 != null && root2 != null){
+            if(root1.val == root2.val){
+                result = DoesTree1HaveTree2(root1, root2);
+            }
+            //如果找不到，那么就再去root的左儿子当作起点，去判断时候包含Tree2
+            if(!result){
+                result = HasSubtree(root1.left, root2);                
+            }
+            if(!result){
+                result = HasSubtree(root1.right, root2);
+            }
+            
+        }
+        return result;
+    }
+    public boolean DoesTree1HaveTree2(TreeNode root1, TreeNode root2){
+        //如果Tree2已经遍历完了都能对应的上，返回true
+        if(root2 == null){
+            return true;
+        }
+        //如果Tree2还没有遍历完，Tree1却遍历完了。返回false,这句一定要写在root2判断的后面，或者要自己写if(root1 == null && root2 != null)
+        if(root1 == null){
+            return false;
+        }  
+        if(root1.val != root2.val){
+            return false;
+        }
+        return DoesTree1HaveTree2(root1.left, root2.left) && DoesTree1HaveTree2(root1.right, root2.right);
+    }
+}
