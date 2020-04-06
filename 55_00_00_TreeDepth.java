1ae3e52282c1a2dc3cdfa9893e5d3bd2c1f16511
wannawannawanna <38182423+wannawannawanna@users.noreply.github.com>
2020-04-06 13:06:55 +0800

    Update 55_00_00_TreeDepth.java

--- 55_00_00_TreeDepth.java
+++ 55_00_00_TreeDepth.java
@@ -49,3 +49,24 @@ public class Solution {
         return depth;
     }
 }
+
+//树的最小深度，最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+//如果一个节点的左子树为空，要看右子树的深度 + 1；
+//如果一个节点的右子树为空，要看左子树的深度 + 1；
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null){
+            return minDepth(root.right) + 1;
+        }
+        if(root.right == null){
+            return minDepth(root.left) + 1;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        int depth = (left <= right)?(left + 1):(right + 1);
+        return depth;
+    }
+}
