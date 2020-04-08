797ce97a7ac7aee9fb98fe1d8768272b20801f12
wannawannawanna <38182423+wannawannawanna@users.noreply.github.com>
2020-04-08 22:36:40 +0800

    Update 03_00_Findingduplicatenumberfromarray.java

--- 03_00_Findingduplicatenumberfromarray.java
+++ 03_00_Findingduplicatenumberfromarray.java
@@ -26,3 +26,41 @@ public class Findingduplicatenumberfromarray {
 		System.out.println(fdna.finddup(num, 7));
 	}
 }
+
+
+
+
+public class Solution {
+    // Parameters:
+    //    numbers:     an array of integers
+    //    length:      the length of array numbers
+    //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
+    //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
+    //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+        if(numbers == null || length <= 0) {
+			return false;
+		} 
+        for(int i = 0; i < length; i++){
+            if(numbers[i] < 0 || length - 1 < numbers[i]){
+                return false;
+            }
+        }
+		for(int i = 0; i < length; i++) {			
+			while(i != numbers[i]) {				
+				if(numbers[i] == numbers[numbers[i]]) {	
+                    duplication[0] = numbers[i];
+					return true;
+				}
+                //swap numbers[i] and numbers[numbers[i]]
+				int temp = numbers[i];
+				numbers[i] = numbers[temp];
+				numbers[temp] = temp;
+
+			}
+		}
+		return false;
+    }
+}
